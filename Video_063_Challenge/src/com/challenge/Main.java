541be5efacf09292a5fbb8980c39ca173098aed9
madus <madalin.mihai.udrea@gmail.com>
2022-05-12 12:21:01 +0300

    Video_063_Challenge For statement method

--- Video_063_Challenge/src/com/challenge/Main.java
+++ Video_063_Challenge/src/com/challenge/Main.java
@@ -7,6 +7,34 @@ public class Main {
 
         printDayOfTheWeek(2);
         printDayOfTheWeek(4);
+        printDayOfTheWeekForStatement(1);
+        printDayOfTheWeekForStatement(2);
+        printDayOfTheWeekForStatement(5);
+        printDayOfTheWeekForStatement(6);
+
+    }
+
+    private static void printDayOfTheWeekForStatement(int day) {
+
+        if (day == 1) {
+
+            System.out.println("Monday");
+
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5){
+            System.out.println("Fiday");
+        } else if (day == 6){
+            System.out.println("Saturday");
+        } else if (day == 7){
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Invalid day");
+        }
 
     }
 
