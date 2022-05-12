c168a497ec0ce674d73b3e071735462f80fa2ba2
madus <madalin.mihai.udrea@gmail.com>
2022-05-12 16:57:00 +0300

    Video_066_sumChallenge

--- Video_066_sumChallenge/src/com/challenge/Main.java
+++ Video_066_sumChallenge/src/com/challenge/Main.java
@@ -1,8 +1,29 @@
 package com.challenge;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int j = 0;
+        int sum = 0;
+        for (int i = 1; i <= 1000; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0){
+
+                System.out.println("The number is: " + i);
+                j++;
+                sum=sum+i;
+            }
+
+            if(j == 5)
+                break;
+
+
+        }
+
+        System.out.println("Suma este " + sum);
+
     }
 }
