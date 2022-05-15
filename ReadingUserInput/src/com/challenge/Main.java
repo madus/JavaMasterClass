1f72ef89a98557aecc69f299ef45e0488d647265
madus <madalin.mihai.udrea@gmail.com>
2022-05-15 17:18:18 +0300

    Video_071_Reading User Input (year and handle next line character

--- ReadingUserInput/src/com/challenge/Main.java
+++ ReadingUserInput/src/com/challenge/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); //handle next line character (enter key)
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
-        System.out.println("Your name is: " + name);
+        int age = 2022 - yearOfBirth;
+        System.out.println("Your name is: " + name + ", you are " + age + " years old");
 
         scanner.close();
     }
