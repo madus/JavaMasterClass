6fb659570720feab1c7bbf00d2ed51bbbeacff99
madus <madalin.mihai.udrea@gmail.com>
2022-05-23 18:02:56 +0300

    Video_073_ReadingUserInputChallenge

--- Video_073_ReadingUserInputChallenge/src/com/challenge/Main.java
+++ Video_073_ReadingUserInputChallenge/src/com/challenge/Main.java
@@ -1,8 +1,34 @@
 package com.challenge;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add number:");
+        int numbers = scanner.nextInt();
+        System.out.println("your number is: " + numbers);
+
+
+       // boolean hasNextInt = scanner.hasNextInt();
+        scanner.nextLine();
+      /* if () {*/
+            int sum = 0;
+            for (int i = 1; i <= numbers; i++) {
+
+                System.out.println("Introduce numarul " + i + ":");
+
+                int addNumber = scanner.nextInt();
+                sum = sum + addNumber;
+
+            }
+
+            System.out.println("Suma numerelor este: " + sum);
+
+      /* }*/
+
+
     }
 }
